7a406a838f61480c897150e15c10d8e0efcf8ae2
samantha <samanthap8675@gmail.com>
2023-10-13 16:51:50 -0400

    first commit

---
+++ src/components/Qr.jsx
@@ -0,0 +1,21 @@
+import React from 'react' 
+
+export default function Qr() {
+    return (
+        <body>
+            <div id = "container">
+                <img id = "qrcode" src="image-qr-code.png"/> 
+                <div id = "textLabels">
+                    <p id = "boldedText">Improve your front-end<br/>skills by building projects</p>
+                    <p id = "nonBoldedText">Scan the QR code to visit Frontend<br />
+                        Mentor and take your coding skills to<br/>the next level
+                    </p>
+
+                </div>
+                
+            </div> 
+        </body>
+        
+        
+    ) ; 
+}
